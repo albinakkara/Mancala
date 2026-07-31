d0dc5e60240d74e0a20f7344c1e6d00b81524e76
Albin Akkara <albinakkara10@gmail.com>
2026-07-31 14:53:47 +0530

    feat: integrate Cloudflare adapter and configure Wrangler deployment settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://onlinemancala.com',
+  adapter: cloudflare(),
   integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
